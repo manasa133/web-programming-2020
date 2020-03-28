8a97c8cae5d1aa93011c52eabbeaaecf825aef45
ManasaMona <manasaalisetty@gmail.com>
2020-03-28 10:37:15 +0530

    auth

--- project1/application.py
+++ project1/application.py
@@ -18,7 +18,6 @@ if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
-# app.config['DATABASE_URL'] = "postgres://arbvdcuultjpji:df906a24f86a440fa38cc93355b33dca9fdde7cfbb58f57494109386328395c6@ec2-18-213-176-229.compute-1.amazonaws.com:5432/dc4c5bip5hbv83"
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -43,18 +42,37 @@ def register():
 		uname = request.form['uname']
 		passw = request.form['passw']
 		time=datetime.now()
-		try:
-			new = User(name=uname, passw=passw,time=time)
-			db.add(new)
-			db.commit()
-		except Exception as e:
-			print(str(e))
-	render_template("register.html")
+		# try:
+		new = User(name=uname, passw=passw,time=time)
+		db.add(new)
+		db.commit()
+		# except Exception as e:
+		# 	print(str(e))
+	return render_template("register.html")
 
 
 @app.route("/admin",methods=['GET','POST'])
 def admin():
 	users = db.query(User)
 	return render_template("admin.html",result = users)
+
+@app.route("/login")
+def login():
+	return render_template("login.html")
+
+@app.route("/auth",methods=['GET','POST'])
+def auth():
+	users = db.query(User)
+	message = ["Login failed"]
+	if request.method == 'POST':
+		uname = request.form['uname']
+		passw = request.form['passw']
+		for user in users:
+			if(user.name==uname):
+				if(user.passw == passw):
+					return render_template("home.html")
+	print("Failed")
+	return render_template("register.html",result=message)
+
 
 
